37f0f779e52fba81ca6fff92024b859e6bca8dd5
DDOSooS <abde.gherofficiel@gmail.com>
2023-03-11 16:27:48 +0100

    home section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 function App() {
   return (
     <>
     <Header/>
+    <main className="mainContent">
+      <Home/>
+    </main>
     </>
   );
 }
---
+++ portfolio/src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Social() {
+  return (
+    <div className="home--social">
+      <a href="#contact" className="social--icon" target="_blank">
+       <i class="uil uil-linkedin"></i> 
+      </a>
+
+      <a href="#contact" className="social--icon" target="_blank">
+        <i class="uil uil-instagram-alt"></i>
+      </a>
+
+      <a href="#contact" className="social--icon" target="_blank">
+        <i class="uil uil-github"></i>
+      </a>
+
+    </div>
+  )
+}
